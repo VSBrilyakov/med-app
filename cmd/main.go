b39037c596878a1a4688755ae62a89a890e769dc
MNOGOHODOVOCHKA <yesitsvova@mail.ru>
2023-01-13 12:42:28 +0300

    Handlers, auth endpoints init

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	medapp "github.com/mnogohoddovochka/med-app"
+	"github.com/mnogohoddovochka/med-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(medapp.Server)
 
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Server start error: %s", err.Error())
 	}
 }
---
+++ pkg/handler/handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	return router
+}
