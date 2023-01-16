05bd4c7aafc987378532da3e6633c3f80ed71d30
MNOGOHODOVOCHKA <yesitsvova@mail.ru>
2023-01-16 15:55:44 +0300

    Connection to db, add db data to config file

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	medapp "github.com/mnogohoddovochka/med-app"
 	"github.com/mnogohoddovochka/med-app/pkg/handler"
 	"github.com/mnogohoddovochka/med-app/pkg/repository"
@@ -15,7 +18,24 @@ func main() {
 		log.Fatalf("error loading config file:%s", err.Error())
 	}
 
-	repo := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables:%s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("database initializing failed:%s", err.Error())
+	}
+
+	repo := repository.NewRepository(db)
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorisation interface {
 }
 
@@ -15,6 +17,6 @@ type Repository struct {
 	PatientList
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
