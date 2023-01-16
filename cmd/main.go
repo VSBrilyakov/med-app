36384b838262907885fb7f774c8a3dc71ae8a4a8
MNOGOHODOVOCHKA <yesitsvova@mail.ru>
2023-01-16 12:54:12 +0300

    Add config file

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/mnogohoddovochka/med-app/pkg/handler"
 	"github.com/mnogohoddovochka/med-app/pkg/repository"
 	"github.com/mnogohoddovochka/med-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error loading config file:%s", err.Error())
+	}
+
 	repo := repository.NewRepository()
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 
 	srv := new(medapp.Server)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Server start error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
